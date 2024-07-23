7c4c6360e548bd8310e714eccfba5ae22cde82cf
boxjod <boxjod@163.com>
2024-07-23 11:22:48 +0800

    second commit

--- install.sh
+++ install.sh
@@ -6,11 +6,12 @@ conda activtae rlbench_rpt
 
 # 2. CoppeliaSim
 # set env variables
-export COPPELIASIM_ROOT=${HOME}/CoppeliaSim
+export COPPELIASIM_ROOT=${HOME}/workspace/CoppeliaSim
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$COPPELIASIM_ROOT
 export QT_QPA_PLATFORM_PLUGIN_PATH=$COPPELIASIM_ROOT
 
 wget https://downloads.coppeliarobotics.com/V4_1_0/CoppeliaSim_Edu_V4_1_0_Ubuntu20_04.tar.xz
+
 mkdir -p $COPPELIASIM_ROOT && tar -xf CoppeliaSim_Edu_V4_1_0_Ubuntu20_04.tar.xz -C $COPPELIASIM_ROOT --strip-components 1
 rm -rf CoppeliaSim_Edu_V4_1_0_Ubuntu20_04.tar.xz
 
