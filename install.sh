3e3957fcdd2beefb9f7d7ed822777ff7663c0b89
boxjod <boxjod@163.com>
2024-07-23 12:00:16 +0800

    requirements update

--- install.sh
+++ install.sh
@@ -1,11 +1,22 @@
-# 1. conda insatll
+# 1. conda insatll and create
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh && bash Miniconda3-latest-Linux-x86_64.sh && rm Miniconda3-latest-Linux-x86_64.sh && source ~/.bashrc
+    # 1.1recommend setting：
+conda config --set auto_activate_base false
+conda install -c conda-forge conda-bash-completion
+    # 1.2add bash command to bashrc
+if [[ -r ~/miniconda3/etc/profile.d/bash_completion.sh ]];then
+	. ~/miniconda3/etc/profile.d/bash_completion.sh
+else 
+	echo "WARNING: could not find conda-bash-completion setup script"
+fi
 
+    # 1.3 create env for RLBench_RPT
 conda create -n rlbench_rpt python=3.8.10 # the version is strict
 conda activtae rlbench_rpt
 
-# 2. CoppeliaSim
-# set env variables
+
+# 2. install CoppeliaSim
+    # set env variables
 export COPPELIASIM_ROOT=${HOME}/workspace/CoppeliaSim
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$COPPELIASIM_ROOT
 export QT_QPA_PLATFORM_PLUGIN_PATH=$COPPELIASIM_ROOT
@@ -13,23 +24,24 @@ export QT_QPA_PLATFORM_PLUGIN_PATH=$COPPELIASIM_ROOT
 wget https://downloads.coppeliarobotics.com/V4_1_0/CoppeliaSim_Edu_V4_1_0_Ubuntu20_04.tar.xz
 mkdir -p $COPPELIASIM_ROOT && tar -xf CoppeliaSim_Edu_V4_1_0_Ubuntu20_04.tar.xz -C $COPPELIASIM_ROOT --strip-components 1
 rm -rf CoppeliaSim_Edu_V4_1_0_Ubuntu20_04.tar.xz
+bash $COPPELIASIM_ROOT/coppeliaSim.sh  
 
-bash /home/ubuntu/data0/liangws/peract_root/CoppeliaSim/coppeliaSim.sh  
 
 # 3. RPT for RLBench 
-# 3.1 git the project
+conda activate rlbench_rpt
+    # 3.1 git the project
 git clone https://github.com/Boxjod/RLBench_RPT.git
-# 3.2 install and test pyrep
+    # 3.2 install and test pyrep
 cd RLBench_RPT/PyRep
 pip3 install -r requirements.txt
 pip3 install -e .
 python examples/example_panda_reach_target.py
-# 3.3 install andtest RLBench
+    # 3.3 install andtest RLBench
 cd RLBench_RPT/RLBench
 pip install -r requirements.txt
 pip install -e .
 python tools/task_builder.py
-# 3.4 test RLBench RPT
+    # 3.4 test RLBench RPT
 cd RLBench_RPT/RPT_Model
 pip install -r requirements.txt
 cd act/detr && pip install -e .
