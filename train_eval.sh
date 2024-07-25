2b3a2e7af4b5025dfd17f7b407369ebb46546043
boxjod <boxjod@163.com>
2024-07-25 19:12:03 +0800

    。 do

--- train_eval.sh
+++ train_eval.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # 生成数据集
-python3 RLBench/tools/dataset_generator_sawyer_act3.py \
-    --save_path Datasets \
-    --tasks push_buttons \
-    --variations 1 \
-    --episodes_per_task 50
+# python3 RLBench/tools/dataset_generator_sawyer_act3.py \
+#     --save_path Datasets \
+#     --tasks push_button \
+#     --variations 1 \
+#     --episodes_per_task 50
 
 model_type=(ACT0E0 ACT3E0 ACT3E2 ACT3E3)
 epoch_list=(8000 9000 10000 11000 12000 13000 14000)
@@ -24,14 +24,14 @@ for model in ${model_type[@]}
         echo '##################################################################'
         
         CUDA_VISIBLE_DEVICES=0 python3 RPT_model/imitate_episodes_sawyer4.py \
-        --task_name push_buttons \
-        --ckpt_dir Trainings \
+        --task_name push_button \
+        --ckpt_dir sorting_program5 \
         --policy_class $model --kl_weight 10 --chunk_size $chunk --hidden_dim 512 --batch_size 8 --dim_feedforward 3200 \
         --num_epochs $epoch  --lr 1e-5 --seed 0 --backbone $backbone \
         ; \
         CUDA_VISIBLE_DEVICES=0 python3 RPT_model/imitate_episodes_sawyer4.py \
-        --task_name push_buttons \
-        --ckpt_dir Trainings \
+        --task_name push_button \
+        --ckpt_dir sorting_program5 \
         --policy_class $model --kl_weight 10 --chunk_size $chunk --hidden_dim 512 --batch_size 8 --dim_feedforward 3200 \
         --num_epochs $epoch  --lr 1e-5 --seed 0 --backbone $backbone \
         --eval --temporal_agg 
