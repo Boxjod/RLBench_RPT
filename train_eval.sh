8019b43954d816f3d6ff305c0532547b60383364
boxjod <boxjod@163.com>
2024-07-30 20:25:37 +0800

    1

--- train_eval.sh
+++ train_eval.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 
-#  超显存了
-# model_type=(ACT3E3)
-# epoch_list=(3000 4000 5000 6000 7000 8000 9000 10000 11000 12000 13000 14000 15000 16000)
-# backbone_list=("efficientnet_b0")
-# chunk_size=(20 30)
-# for model in ${model_type[@]}
-#   do
-#   for epoch in ${epoch_list[@]}
-#     do
-#     for backbone in ${backbone_list[@]}
-#       do
-#       for chunk in ${chunk_size[@]}
-#         do
-#         echo '##################################################################'
-#         echo 'train on model=' $model ', epoch=' $epoch ',  chunk_size='$chunk 
-#         echo '##################################################################'
+model_type=(ACT3E3)
+epoch_list=(3000 4000 5000 6000 7000 8000 9000 10000 11000 12000 13000 14000 15000 16000)
+backbone_list=("efficientnet_b0")
+chunk_size=(20 30)
+for model in ${model_type[@]}
+  do
+  for epoch in ${epoch_list[@]}
+    do
+    for backbone in ${backbone_list[@]}
+      do
+      for chunk in ${chunk_size[@]}
+        do
+        echo '##################################################################'
+        echo 'train on model=' $model ', epoch=' $epoch ',  chunk_size='$chunk 
+        echo '##################################################################'
         
-#         CUDA_VISIBLE_DEVICES=0 python3 RPT_model/imitate_episodes_sawyer4.py \
-#         --task_name basketball_in_hoop \
-#         --ckpt_dir Trainings \
-#         --policy_class $model --kl_weight 10 --chunk_size $chunk --hidden_dim 512 --batch_size 8 --dim_feedforward 3200 \
-#         --num_epochs $epoch  --lr 1e-5 --seed 0 --backbone $backbone \
-#         ; \
-#         CUDA_VISIBLE_DEVICES=0 python3 RPT_model/imitate_episodes_sawyer4.py \
-#         --task_name basketball_in_hoop \
-#         --ckpt_dir Trainings \
-#         --policy_class $model --kl_weight 10 --chunk_size $chunk --hidden_dim 512 --batch_size 8 --dim_feedforward 3200 \
-#         --num_epochs $epoch  --lr 1e-5 --seed 0 --backbone $backbone \
-#         --eval --temporal_agg 
-#         done
-#       done
-#     done
-#   done
+        CUDA_VISIBLE_DEVICES=0 python3 RPT_model/imitate_episodes_sawyer4.py \
+        --task_name basketball_in_hoop \
+        --ckpt_dir Trainings \
+        --policy_class $model --kl_weight 10 --chunk_size $chunk --hidden_dim 512 --batch_size 8 --dim_feedforward 3200 \
+        --num_epochs $epoch  --lr 1e-5 --seed 0 --backbone $backbone \
+        ; \
+        CUDA_VISIBLE_DEVICES=0 python3 RPT_model/imitate_episodes_sawyer4.py \
+        --task_name basketball_in_hoop \
+        --ckpt_dir Trainings \
+        --policy_class $model --kl_weight 10 --chunk_size $chunk --hidden_dim 512 --batch_size 8 --dim_feedforward 3200 \
+        --num_epochs $epoch  --lr 1e-5 --seed 0 --backbone $backbone \
+        --eval --temporal_agg 
+        done
+      done
+    done
+  done
 
 # 生成数据集
 python3 RLBench/tools/dataset_generator_sawyer_act3.py \
