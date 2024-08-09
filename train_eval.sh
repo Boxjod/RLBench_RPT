e18cb7c529ce87dd2f5a1848714e17753c2bca10
boxjod <boxjod@163.com>
2024-08-09 14:14:17 +0800

    eval

--- train_eval.sh
+++ train_eval.sh
@@ -9,7 +9,7 @@
 #     --episodes_per_task 50
 
 task_name=("meat_off_grill" )
-model_type=(ACT0E0 ACT3E0 ACT3E2 ACT3E3)
+model_type=(ACT3E0)
 epoch_list=(1000 2000 3000 4000 5000 6000 7000 8000 9000 10000 11000 12000 13000 14000 15000 16000)
 backbone_list=("resnet18")
 chunk_size=(30)
